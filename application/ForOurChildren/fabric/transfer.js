f69e7ba805c6e8fd596e64e1ed3629419d1d3902
2namkyung <iddbxk@daum.net>
2021-08-06 22:54:24 +0900

    rollback chaincode, not seperate

--- application/ForOurChildren/fabric/transfer.js
+++ application/ForOurChildren/fabric/transfer.js
@@ -37,7 +37,7 @@ async function main() {
         const network = await gateway.getNetwork('mychannel');
 
         // Get the contract from the network.
-        const contract = network.getContract('children_tx');
+        const contract = network.getContract('children');
 
         // Submit the specified transaction.
 	//await contract.submitTransaction('TransferAsset', 'Joo', 'Lee', 50000);
