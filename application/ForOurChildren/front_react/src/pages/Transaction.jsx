2b453295683b6a550391de4a274d1aa97810da46
2namkyung <iddbxk@daum.net>
2021-09-23 17:09:00 +0900

    children transaction pagination

--- application/ForOurChildren/front_react/src/pages/Transaction.jsx
+++ application/ForOurChildren/front_react/src/pages/Transaction.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
+import Pagination from '../js/Pagination';
 
 export default function Transaction() {
     const [results, setResult] = useState([]);
+
+    // pagination
+    const [currentPage, setCurrentPage] = useState(1);
+    const [postsPerPage] = useState(10);
 
     useEffect(() => {
         fetch("http://localhost:4000/" + window.location.pathname, {
@@ -11,47 +16,17 @@ export default function Transaction() {
             headers: { 'Content-Type': 'application/json' }
         })
             .then((response) => response.json())
-            .then((jsonData) => setResult(jsonData))
+            .then((jsonData) => {
+                setResult(jsonData);
+            });
     }, []);
 
-    const resultList = () => {
-        const result = [];
-        for (let i = 0; i < results.length; i++) {
-            console.log(results[i].Value);
-            const obj = JSON.parse(results[i].Value);
-            if (obj.receiver === undefined) {
-                const url = window.location.pathname;
-                const name = url.substr(url.lastIndexOf('/') + 1);
-                obj.receiver = name;
-                obj.sender = "지원센터"
-                obj.amount = obj.coin;
-                obj.time = "최초지급일";
-            }
-            result.push(
-                <tr key={i}>
-                    <td>
-                        {i + 1}
-                    </td>
-                    <td>
-                        {obj.receiver}
-                    </td>
-                    <td>
-                        {obj.amount}
-                    </td>
-                    <td>
-                        {obj.sender}
-                    </td>
-                    <td>
-                        {obj.coin}
-                    </td>
-                    <td>
-                        {obj.time}
-                    </td>
-                </tr>
-            )
-        }
-        return result;
-    }
+
+    const indexOfLast = currentPage * postsPerPage;
+    const indexOfFirst = indexOfLast - postsPerPage;
+    const currentPosts = results.slice(indexOfFirst, indexOfLast);
+
+    console.log(currentPosts);
 
     return (
         <>
@@ -69,9 +44,47 @@ export default function Transaction() {
                             </tr>
                         </thead>
                         <tbody>
-                            {resultList()}
+                            {currentPosts.map((result, index) => {
+                                const obj = JSON.parse(result.Value);
+                                if (obj.receiver === undefined) {
+                                    const url = window.location.pathname;
+                                    const name = url.substr(url.lastIndexOf('/')+1);
+                                    const names = name.split("%20");
+                                    if(names[1]===undefined){
+                                        obj.receiver = names[0];
+                                    }else{
+                                        obj.receiver = names[0] + " " + names[1];
+                                    }
+                                    obj.sender = "지원센터";
+                                    obj.amount = obj.coin;
+                                    obj.time = "최초지급일";
+                                }
+                                return (
+                                    <tr>
+                                        <td className="TxID">
+                                            {result.TxId}
+                                        </td>
+                                        <td>
+                                            {obj.receiver}
+                                        </td>
+                                        <td>
+                                            {obj.amount}
+                                        </td>
+                                        <td>
+                                            {obj.sender}
+                                        </td>
+                                        <td>
+                                            {obj.coin}
+                                        </td>
+                                        <td>
+                                            {obj.time}
+                                        </td>
+                                    </tr>
+                                )
+                            })}
                         </tbody>
                     </table>
+                    <Pagination postsPerPage={postsPerPage} totalPosts={results.length} paginate={setCurrentPage}/>
                 </div>
             </div>
         </>
