7fa19d0e7c8066404de08ea17a05bc0e3dd0a0ad
2namkyung <iddbxk@daum.net>
2021-09-23 14:10:18 +0900

    transaction page : change class Component --> function Component

--- application/ForOurChildren/front_react/src/pages/Transaction.jsx
+++ application/ForOurChildren/front_react/src/pages/Transaction.jsx
@@ -1,67 +1,60 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
-class Transaction extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            data: []
-        }
-    }
+export default function Transaction() {
+    const [results, setResult] = useState([]);
 
-    componentDidMount() {
+    useEffect(() => {
         fetch("http://localhost:4000/" + window.location.pathname, {
             method: "GET",
             headers: { 'Content-Type': 'application/json' }
         })
             .then((response) => response.json())
-            .then((jsonData) =>
-                this.setState({
-                    data: jsonData
-                }));
+            .then((jsonData) => setResult(jsonData))
+    }, []);
+
+    const resultList = () => {
+        const result = [];
+        for (let i = 0; i < results.length; i++) {
+            console.log(results[i].Value);
+            const obj = JSON.parse(results[i].Value);
+            if (obj.receiver === undefined) {
+                const url = window.location.pathname;
+                const name = url.substr(url.lastIndexOf('/') + 1);
+                obj.receiver = name;
+                obj.sender = "지원센터"
+                obj.amount = obj.coin;
+                obj.time = "최초지급일";
+            }
+            result.push(
+                <tr key={i}>
+                    <td>
+                        {i + 1}
+                    </td>
+                    <td>
+                        {obj.receiver}
+                    </td>
+                    <td>
+                        {obj.amount}
+                    </td>
+                    <td>
+                        {obj.sender}
+                    </td>
+                    <td>
+                        {obj.coin}
+                    </td>
+                    <td>
+                        {obj.time}
+                    </td>
+                </tr>
+            )
+        }
+        return result;
     }
 
-    render() {
-        const data = this.state.data;
-        // console.log(data);
-        const resultList = () => {
-            const result = [];
-            for (let i = 0; i < data.length; i++) {
-                console.log(data[i].Value);
-                const obj = JSON.parse(data[i].Value);
-                if (obj.receiver === undefined){
-                    const url = window.location.pathname;
-                    const name = url.substr(url.lastIndexOf('/')+1);
-                    obj.receiver = name;
-                    obj.sender = "지원센터"
-                    obj.amount = obj.coin;
-                    obj.time = "최초지급일";
-                }
-                    result.push(
-                        <tr key={i}>
-                            <td>
-                                {i + 1}
-                            </td>
-                            <td>
-                                {obj.receiver}
-                            </td>
-                            <td>
-                                {obj.amount}
-                            </td>
-                            <td>
-                                {obj.sender}
-                            </td>
-                            <td>
-                                {obj.coin}
-                            </td>
-                            <td>
-                                {obj.time}
-                            </td>
-                        </tr>
-                    )
-            }
-            return result;
-        }
-        return (
+    return (
+        <>
             <div>
                 <div className="content">
                     <table>
@@ -81,9 +74,6 @@ class Transaction extends React.Component {
                     </table>
                 </div>
             </div>
-        );
-    }
-
+        </>
+    )
 }
-
-export default Transaction;
