e8850f4bafde08af727766fdbdf85fb0871f7a6a
2namkyung <iddbxk@daum.net>
2021-11-11 14:46:08 +0900

    2021-11-11 update only source code

---
+++ front/src/pages/Header.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import LoginCheck from '../components/LoginCheck';
+import Badges from '../components/Badges';
+
+export default function Header(){
+
+    return (
+        <div className="header">
+            <div className="inner">
+                <div className="header_nav">
+                    <div className="home">
+                        <Link to="/" className="link_home">Korean Clean Food</Link>
+                    </div>
+                    <div className="sub_menu">
+                        <LoginCheck/>
+                        <ul>
+                            <li>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li>
+                                <Link to="/user">Children</Link>
+                            </li>
+                            <li>
+                                <Link to="/explorer">Explorer</Link>
+                            </li>
+                            <li>
+                                <Link to="/transactionLogAll">Transaction</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <Badges />
+        </div>
+    );
+}
---
+++ front/src/pages/UserList.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+class UserList extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: []
+        }
+    }
+
+    componentDidMount() {
+        fetch("http://localhost:4000/getAllInfo", {
+            method: "GET",
+            headers: { 'Content-Type': 'application/json' }
+        })
+            .then((response) => response.json())
+            .then((jsonData) =>
+                this.setState({
+                    data: jsonData
+                }));
+        // console.log(jsonData));
+    }
+
+    render() {
+        const { data } = this.state;
+        const resultList = () => {
+            const result = [];
+            for (let i = 0; i < data.length; i++) {
+                // console.log(data[i].Record);
+                // if(data[i].Key.indexOf("TxLog")===0){
+                //     continue;
+                // }
+                result.push(
+                    <tr key={i}>
+                        <td>
+                            <div className="data">
+                                <Link to={`/getTransaction/${data[i].Key}`}>{data[i].Key}</Link>
+                            </div>
+                        </td>
+                        <td>
+                            <div className="data">
+                                {data[i].Record.coin}
+                            </div>
+
+                        </td>
+                    </tr>
+                )
+            }
+            return result;
+        }
+        return (
+            <div className="inner">
+                <div className="content">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>이름</th>
+                                <th>Amount</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {resultList()}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default UserList;
